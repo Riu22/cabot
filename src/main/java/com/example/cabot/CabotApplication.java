d6b41733b28379d1326e47c0b2b9fd3b28b46d22
riu22 <riutordjaume@gmail.com>
2026-03-16 13:10:34 +0100

    first commit

---
+++ src/main/java/com/example/cabot/CabotApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cabot/CabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
